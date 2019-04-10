8b5262b1164544647618f6206166d2b36abb943a
XXC123 <s3561226@student.rmit.edu.au>
2019-04-10 13:12:34 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
   test "should get root" do
-    get FILL_IN 
-    assert_response FILL_IN 
-  end
+    get FILL_IN
+    assert_response FILL_IN
+    end
 
   test "should get home" do
     get static_pages_home_url
@@ -19,6 +18,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get about" do
     get static_pages_about_url 
-    assert_response :success 
+    assert_response :success
   end 
 end
